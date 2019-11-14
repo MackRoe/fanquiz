2db0cd2cb1c2656ff76acaabfe5c52760beaae8c
MackRoe <elaine.music@students.makeschool.com>
2019-11-14 14:16:52 -0800

    first commit. project and app initialized

---
+++ fanquiz/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fanquiz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanquiz.settings')
+
+application = get_wsgi_application()
